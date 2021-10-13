21296bcb619ac6b4b44c002bda100114eb56fa37
achavez99 <73123090+achavez99@users.noreply.github.com>
2021-10-13 20:10:46 +0200

    aaaaaa

---
+++ MP07/PHP/Registerlogin/login.php
@@ -0,0 +1,32 @@
+<?php
+$mail = $_POST['Mail'];
+$contraseña = $_POST['Contraseña'];
+
+/* CONFIGURAR CONEXION AL SERVIDOR*/
+
+$servidor="localhost";
+$usuario="root";
+$password="usbw";
+$register="registerlogin";
+
+/* CONEXION A BASE DE DATOS VARIABLE CONEXION*/
+
+$con=mysqli_connect($servidor,$usuario,$password,$register);
+ if(!$con){
+    die("No se ha podido realizar la conexión ".mysqli_connect_error()."<br>");
+ }else{
+     mysqli_set_charset($con,"utf8");
+     echo "Se ha establecido correctamente la conexión a la base de datos";
+    
+     $sql="INSERT INTO `registerlogin`.`registerlogin` (`id`, `Nombre`, `Apellidos`, `Mail`, `Contraseña`, `ConfContraseña`) 
+     VALUES (NULL, '".$nombre."', '".$apellidos."', '".$mail."', '".$contraseña."', '".$confContraseña."')";
+
+     $consulta=mysqli_query($con,$sql);
+
+     if(!$con){
+         die("No se ha podido realizar el insert");
+     }else{
+         echo "El insert se ha realizado correctamente";
+     }
+    }
+?>
